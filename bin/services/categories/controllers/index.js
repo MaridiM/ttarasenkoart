612bc40f69f862187786fbbc3c2959856c15cf30
MaridiM <maridim92@gmail.com>
2021-09-20 14:16:02 +0300

    Error with  JSON build

--- bin/services/categories/controllers/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CategoryController = void 0;
-var categories_1 = require("./categories");
-Object.defineProperty(exports, "CategoryController", { enumerable: true, get: function () { return categories_1.CategoryController; } });
-//# sourceMappingURL=index.js.map
