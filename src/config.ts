d1c188bf402118ba228cd19af8d90aeec40e32f9
MaridiM <maridim92@gmail.com>
2021-09-20 14:48:31 +0300

    test api server

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,5 @@ export const REACT_APP_CLOUDINARY_API_KAY: string = process.env.REACT_APP_CLOUDI
 export const REACT_APP_CLOUDINARY_API_SECRET: string = process.env.REACT_APP_CLOUDINARY_API_SECRET || 'RXcPZVDEzyS9ynNC-VAmlDK78IQ'
 export const REACT_APP_CLOUDINARY_UPLOAD_PRESET: string = process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET || 'pxrbylo3_ttarasenkoart.com'
 export const REACT_APP_CLOUDINARY_API_URL: string = process.env.REACT_APP_CLOUDINARY_API_URL || 'https://api.cloudinary.com/v1_1/dki4lxdki/image/upload'
+
+export const SERVER_API: string = 'https://server-ttarasenkoart.herokuapp.com/'
--- src/paths.ts
+++ src/paths.ts
@@ -1,11 +1,11 @@
 import { TPaths, TPathsAPI } from "types"
 
 export const paths: TPaths = {
-    admin: '/admin',
-    gallery: '/admin/gallery',
-    add: '/admin/gallery/p/add',
-    picture: (id?: string | number): string => id ? `/admin/p/${id}` : '/admin/p/:id',
-    edit: (id?: string | number): string => id ? `/admin/gallery/p/${id}` : '/admin/gallery/p/:id',
+    admin: '/',
+    gallery: '/gallery',
+    add: '/gallery/p/add',
+    picture: (id?: string | number): string => id ? `/p/${id}` : '/p/:id',
+    edit: (id?: string | number): string => id ? `/gallery/p/${id}` : '/gallery/p/:id',
 }
 
 const baseApiPath: string = '/api'
